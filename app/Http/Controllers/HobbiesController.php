e3dcec6ea68a80e3ddfe6a2721d5099cf0597a60
Jhon Lester Sadang <lestermillaresadang@gmail.com>
2024-02-16 11:53:01 +0800

    controllers added

--- app/Http/Controllers/HobbiesController.php
+++ app/Http/Controllers/HobbiesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Skills extends Controller
+class HobbiesController extends Controller
 {
     //
 }
--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Hobbies extends Controller
+class SkillsController extends Controller
 {
     //
 }
